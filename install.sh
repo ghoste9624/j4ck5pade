a7da245b9e6c0ec69870b7c3036bc35765ff89a2
ghoste9624 <jackspade44@outlook.com>
2025-04-20 19:45:56 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+apt update && apt upgrade -y && apt install git python termux-api lynx lychee traceroute whois nmap ruby -y
+cd $HOME
+git clone https://github.com/CPScript/Reaper
+cd Reaper
+pip install -r requirements.txt
+cd $HOME
+pip install google
+pip install linkook
+pip install socialscan
+pip install --upgrade ipwhois
+pip install -U search4 
+pip install --upgrade netizenship
+pip install nexfil
+pip install holehe
+
+pkg update && pkg upgrade -y && apt autoremove && apt autoclean -y

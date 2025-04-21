23411957e0cec26052f2dfa62adabe72cd47badd
ghoste9624 <jackspade44@outlook.com>
2025-04-20 21:18:45 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ cd $HOME
 git clone https://github.com/CPScript/Reaper
 cd Reaper
 pip install -r requirements.txt
+
 cd $HOME
+
 pip install google
 pip install linkook
 pip install socialscan
@@ -12,5 +14,6 @@ pip install -U search4
 pip install --upgrade netizenship
 pip install nexfil
 pip install holehe
+pip install setuptools 
 
 pkg update && pkg upgrade -y && apt autoremove && apt autoclean -y

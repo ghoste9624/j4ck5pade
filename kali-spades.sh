7492933078e21943bf92eece750a1b20005963e6
ghoste9624 <jackspade44@outlook.com>
2025-04-25 14:56:37 -0400

    Create kali-spades.sh

---
+++ kali-spades.sh
@@ -0,0 +1,205 @@
+#!/bin/bash
+
+cd $HOME
+
+echo -e "\e[1;91m⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠞⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡰⢫⣼⢷⡘⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠞⣡⣿⡇⢘⣿⣎⠳⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⢋⡾⢇⣤⡡⠜⣤⣸⢷⡙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠞⣴⡟⢳⠞⢷⣦⣱⡾⠛⣞⢻⣦⡹⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠞⣡⡞⢧⣜⣡⠼⠶⠿⠻⠶⢦⣌⡫⡼⢳⣌⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⢣⣾⢏⣹⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣮⣹⣷⡜⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⢋⣴⡟⢳⡾⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣷⡞⢻⣧⡝⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⢋⣴⡿⢧⡼⠋⣼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣧⠙⢧⡼⢻⣦⡙⢦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠞⣱⡿⠋⣹⠞⢵⢡⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣄⠀⢙⡎⡮⠻⣎⠙⢿⣮⠳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⢋⣼⠟⠀⢰⣿⣾⣗⣼⣇⢠⣾⣷⣦⣄⡀⠀⠀⢲⣾⣿⣿⣿⡧⢸⡧⣺⣷⣾⡇⠀⠹⣷⡘⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                                                                      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇⣾⡟⢀⣾⢸⣿⣉⣿⡁⣿⠀⢻⣿⣿⡟⠁⣠⡀⠘⣿⣿⣿⣿⠃⣸⣉⣾⣉⣿⡇⣶⡀⢹⣿⣸⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡾⢰⣿⠃⣸⠛⢦⣙⣛⣩⣵⣿⣦⡀⠉⠉⠀⠀⠟⢇⠀⠈⠉⠉⣁⣤⣿⣿⣜⣛⣋⡴⠛⣧⠀⣿⡇⢷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⣼⣿⠀⣿⠸⢡⣾⣭⠿⠯⣝⣿⣿⢆⠀⡀⠀⢠⠠⠤⠀⣠⣻⣿⡿⣻⠽⠿⢯⣿⣆⠇⢿⠀⢸⣧⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⢺⣿⠀⣿⠀⣿⣿⣿⣉⣿⣾⣣⢿⡌⣷⡷⢶⠾⠞⡿⢿⡇⣿⣿⢹⣱⣿⣉⡷⣻⣿⠀⣿⠀⣸⡟⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⠸⣿⡆⠘⣧⡙⣾⡷⣾⣿⣿⠻⣼⣧⣉⣀⠈⠀⠀⠁⢀⣁⣿⣧⢻⢻⣿⣿⡾⣿⢏⣼⠇⢀⣿⠇⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢧⡙⣿⣦⡈⠛⠾⠿⠿⠛⠁⣰⡿⢣⠮⣉⣽⣿⣿⣯⣉⠵⣌⢿⣦⠊⢙⠿⠿⠟⠛⢁⣠⣿⢏⡼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⣙⠻⢷⣶⣤⣶⣶⣟⣫⣤⡶⠾⢛⢹⣿⣿⡏⡛⠿⢶⣤⣭⣻⣶⣦⣤⣶⡶⠿⣋⡴⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠲⠦⠬⣭⣭⡭⠥⠶⠖⠛⡟⣼⡇⢺⣧⢸⡛⠲⠶⠬⢭⣭⡭⠥⠶⠖⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⢡⣿⠅⠨⣿⣆⢷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⣡⣿⣷⡞⢻⣾⣿⣌⢧⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠏⠴⠿⠼⠟⣛⢛⠛⠧⠿⠦⡙⢆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                                                                      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣞⡥⠶⠒⠛⠉⠉⠉⠉⠉⠉⠛⠒⠶⢬⣳⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+while true; do
+echo -e "\e[0;94m________________________________________
+
+\e[1;91m --==[ Information Gathering
+\e[1;91m --==[ j4ck5pade
+\e[1;91m --==[ https://github.com/ghoste9624
+\e[0;94m________________________________________\e[1;92m"
+  echo -e ""
+  echo -e "  \e[0;94m[\e[0;1m01\e[0;94m] \e[1;91mDomain Info"
+  echo -e "  \e[0;94m[\e[0;1m02\e[0;94m] \e[1;91mSub-Domain Finder/Wildcard"
+  echo -e "  \e[0;94m[\e[0;1m03\e[0;94m] \e[1;91mIP Tracer"
+  echo -e "  \e[0;94m[\e[0;1m04\e[0;94m] \e[1;91mScript-trace/Headers"
+  echo -e "  \e[0;94m[\e[0;1m05\e[0;94m] \e[1;91mTraceroute"
+  echo -e "  \e[0;94m[\e[0;1m06\e[0;94m] \e[1;91mWebscan"
+  echo -e "  \e[0;94m[\e[0;1m07\e[0;94m] \e[1;91mLink Checker"
+  echo -e "  \e[0;94m[\e[0;1m08\e[0;94m] \e[1;91mDNS Lookup"
+  echo -e "  \e[0;94m[\e[0;1m09\e[0;94m] \e[1;91mSubnet Calculator"
+  echo -e "  \e[0;94m[\e[0;1m10\e[0;94m] \e[1;91mUser Search"
+  echo -e "  \e[0;94m[\e[0;1m11\e[0;94m] \e[1;91mOpen Browser"
+  echo -e "  \e[0;94m[\e[0;1m12\e[0;94m] \e[1;91mCrawler/robots.txt"
+  echo -e "  \e[0;94m[\e[0;1m13\e[0;94m] \e[1;91mTLS/SSL Certificates"
+  echo -e "  \e[0;94m[\e[0;1m14\e[0;34m] \e[1;91mSearch Google"
+  echo -e "  \e[0;94m[\e[0;1m15\e[0;34m] \e[1;91mSecurity Check"
+  echo -e "  \e[0;94m[\e[0;1m00\e[0;34m] \e[1;91mExit"
+
+  read -p "Enter your choice: " choice
+
+  case $choice in
+
+    1)
+      echo "Enter Domain..."
+      # Add code for option 1 here
+      read -p " Target: " domain
+      content=$(curl -s "$domain")
+      whois $domain
+
+      ;;
+    2)
+      echo "Enter Domain..."
+      # Add code for option 2 here
+      read -p " Target: " url
+      content=$(curl -s "$url")
+      assetfinder $url
+      dnsrecon -d $url -v -f -a -s -k -z
+
+      ;;
+    3)
+      echo "Executing IP Tracer..."
+      # Add code for option 3 here
+      cd $HOME && cd ipmux && sh ipmux.sh
+      cd $HOME && cd j4ck5pade
+
+      ;;
+    4)
+      echo "Enter Domain..."
+      # Add code for option 4 here
+      read -p " Target: " url
+      content=$(curl -s "$url")
+      nmap -sV -A $url --script-trace --script=http-headers --unprivileged -o $url
+      echo ""
+      echo -e  "  \e[1;93mFiles saved in home directory..."
+
+      ;;
+    5)
+      echo "Enter Domain/IP"
+      # Add code for option 5 here
+      read -p " Target: " url
+      content=$(curl -s "$url")
+      traceroute $url
+   
+      ;;
+    6)
+      echo "Enter Url..."
+      # Add code for option 6 here
+      read -p " Target: " url
+      whatweb --verbose --aggression 3 $url --user-agent --plugins --cookie --cookie-jar
+      echo ""
+
+      ;;
+    7)
+      echo "Enter Link/Url..."
+      # Add code for option 7 here
+      read -p " Target: " url
+      content=$(curl -s "$url")
+      linkchecker $url
+
+      ;;
+    8)
+      echo "Enter Domain..."
+      # Add code for option 8 here
+      read -p " Target: " url
+      content=$(curl -s "$url")
+      dnstwist $url -a -b -g -m -r -w
+
+      ;;
+    9)
+      echo "Enter Domain..."
+      # Add code for option 9 here
+      read -p " Target: " url
+      content=$(curl -s "url")
+      subnetcalc $url
+
+       ;;
+    10)
+      echo "Enter Username..."
+      # Add code for option 10 here
+      read -p " Target: " url
+      content=$(curl -s "$url")
+      sherlock $url -v
+      echo ""
+      echo -e  "  \e[1;93mFiles saved in home directory..."
+
+       ;;
+    11)
+      echo "Enter Url..."
+      # Add code for option 11 here
+      read -p " Target: " url
+      content=$(curl -s "$url")
+      python3 -m webbrowser $url
+
+       ;;
+    12)
+      echo "Enter Url..."                                                                                                                                    # Add code for option 12 here
+      read -p " Target: " url
+      content=$(curl -s "$url")
+      photon -u $url -v
+      echo ""
+      echo -e  "  \e[1;93mFiles saved in home directory..."
+
+       ;;
+    13)
+      echo "Enter Domain/IP..."
+      # Add code for option 13 here
+      read -p " Target: " url
+      content=$(curl -s "$url")
+      tlssled $url 443
+      sslscan $url
+      sslyze $url
+      echo ""
+    echo -e  "  \e[1;93mFiles saved in home directory..."
+
+       ;;
+    14)
+      echo "Searching Google..."
+      # Add code for option 14 here
+      command=(lynx google.com)
+
+           "${command[@]}"
+      ;;
+    15)
+      echo "Checking your Information..."
+      # Add code for option 15 here
+      command=(curl ipinfo.io)
+
+           "${command[@]}"
+       ;;
+    00)
+      # Add fortune here
+      command=(display-dhammapada)
+
+           "${command[@]}"
+
+      echo " Exiting..."
+      break
+
+       ;;
+     *)
+      echo " Invalid choice. Please try again."
+       ;;
+  esac
+done
